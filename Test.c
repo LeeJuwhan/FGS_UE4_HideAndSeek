08e29153c28892192e3cd48dc39b403102269873
최하늘 <gharlic@kut.ac.kr>
2016-10-10 19:43:03 +0900

    충돌테스트1

---
+++ Test.c
@@ -0,0 +1,18 @@
+void main() {
+int a = 10;
+}
+
+
+void func1() {
+	int b = 10;
+}
+
+void func2() {
+	int c = 10;
+}
+
+
+// 현재 버전이 버전1임
+// 내가 func1을 수정하고 push안하고 기다릴테니
+// 딴 사람이 func2를 수정하고 push하셈
+// 어떻게 충돌나나 좀 보게
